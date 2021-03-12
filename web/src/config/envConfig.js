3119a85688c9c3b0380f28bcbecd14af72906de2
Peter <peter.lazarov94@gmail.com>
2021-03-12 17:48:09 +0200

    empty logger service removed

--- web/src/config/envConfig.js
+++ web/src/config/envConfig.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env)
 export default {
     domain: process.env.REACT_APP_DOMAIN,
     apiAddress: process.env.REACT_APP_API_ADDRESS,
--- web/src/services/http.js
+++ web/src/services/http.js
@@ -1,47 +1,24 @@
-import logger from './logger';
 import apiRoutes from '../config/apiRoutes';
-import envConfig from '../config/envConfig';
 
 export default {
     async request(url, options) {
         const verb = (options && options.verb) ? options.verb : 'GET';
         const body = (options && options.body) ? options.body : null;
 
-        if (envConfig.environment === 'development') {
-            logger.log(`fetching ${apiRoutes.ADDRESS}${url} via ${verb}`);
-            if (body) {
-                logger.logObject(body);
-            }
-        }
-
         let response = {};
+        let json = {};
         try {
             response = await fetch(`${apiRoutes.ADDRESS}${url}`, {
                 method: verb,
                 protocol: 'http',
                 headers: {
-                    // 'Access-Control-Allow-Origin': envConfig.domain,
                     'Content-Type': 'application/json',
                 },
                 body: body ? JSON.stringify(body) : null
             });
-            if (envConfig.environment === 'development') {
-                logger.log('Raw responce');
-                logger.logObject(response);
-            }
-        } catch (error) {
-            logger.log(error);
-        }
-
-        let json = {};
-        try {
             json = await response.json();
-            if (envConfig.useHttpLogging) {
-                logger.log('Parsed responce');
-                logger.logObject(json);
-            }
         } catch (error) {
-            logger.log(error);
+            console.log(error);
         }
 
         return {
